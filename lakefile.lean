6df46f880c4393f409d376f51ac18af60c218dc6
Jonathan ƒ(n) Reed <jonathanalanreed1983@gmail.com>
2026-08-24 06:04:33 -0400

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -3,6 +3,9 @@ open Lake DSL
 
 package «EinsteinQuasicrystals» where
 
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
 @[default_target]
-lean_lib «EinsteinQuasicrystals» where
+lean_lib «EinsteinQuasicrystalKernelTriviality» where
   srcDir := "."
